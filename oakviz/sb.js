a35ef17dd8113cf658c52e00af8a92f327433ea6
kashyaba <iamkashyab@gmail.com>
2024-06-06 02:31:56 -0700

    skybox with autorotate working

--- oakviz/sb.js
+++ oakviz/sb.js
@@ -1,10 +1,41 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
+
+// Create the scene
+const scene = new THREE.Scene();
+
+// Create the camera
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 5;
+
+// Create the renderer
+const renderer = new THREE.WebGLRenderer();
+const controls = new OrbitControls( camera, renderer.domElement );
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
 const loader = new THREE.CubeTextureLoader();
 loader.setPath('data/'); // Set the path to your images
 
 const texture = loader.load([
-    'posx.jpg', 'negx.jpg',
-    'posy.jpg', 'negy.jpg',
-    'posz.jpg', 'negz.jpg'
+    'px.png', 'nx.png',
+    'py.png', 'ny.png',
+    'pz.png', 'nz.png'
 ]);
+// texture.mapping = THREE.EquirectangularRefractionMapping
+
+scene.background = texture
+
+// Animation function
+function animate() {
+    requestAnimationFrame(animate);
+    controls.autoRotate = true
+    controls.autoRotateSpeed = 5
+    controls.update()
+    renderer.render(scene, camera);
+}
+
+animate();
+
+
